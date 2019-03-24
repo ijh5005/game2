6de8d9ae3a4f41ba7cca0291bc1f3177f164d159
Isaiah Harrison <isaiahharrison@f0-18-98-15-42-a5.wireless.temple.edu>
2019-03-24 17:17:46 -0400

    big change: changed the way the boxes explode to use the json ref

--- js/animalExplosions.js
+++ js/animalExplosions.js
@@ -11,42 +11,32 @@ const animalExplosions = {
       */
 
       // cache every box that will be exploded
-      const boxNumber = parseInt(box.replace("box", ""));
-      const topRightBoxNumber = boxInfo.getTopRightBoxNumber(boxNumber);
-      const topLeftBoxNumber = boxInfo.getTopLeftBoxNumber(boxNumber);
-      const bottomRightBoxNumber = boxInfo.getBottomRightBoxNumber(boxNumber);
-      const bottomLeftBoxNumber = boxInfo.getBottomLeftBoxNumber(boxNumber);
-      const topBox = boxInfo.getTopBox(boxNumber);
-      const leftBox = boxInfo.getLeftBox(boxNumber);
-      const bottomBox = boxInfo.getBottomBox(boxNumber);
-      const rightBox = boxInfo.getRightBox(boxNumber);
+      const temp = lionSquares[box].filter(data => data !== null);
+      const boxesToExplode = temp.map(data => `box${data}`);
 
-      // places those exploding boxes into an array and filter out invalid boxes
-      // invalid boxes include boxes that are on the edge of the board
-      const boxesToExplode = [
-        `box${boxNumber}`,
-        topRightBoxNumber,
-        topLeftBoxNumber,
-        bottomRightBoxNumber,
-        bottomLeftBoxNumber,
-        topBox,
-        leftBox,
-        bottomBox,
-        rightBox
-      ].filter(data => data);
+      const topLeftBoxNumber = lionSquares[box][0];
+      const topBox = lionSquares[box][1];
+      const topRightBoxNumber = lionSquares[box][2];
+      const leftBox = lionSquares[box][3];
+      const thisBox = lionSquares[box][4];
+      const rightBox = lionSquares[box][5];
+      const bottomLeftBoxNumber = lionSquares[box][6];
+      const bottomBox = lionSquares[box][7];
+      const bottomRightBoxNumber = lionSquares[box][8];
 
       // match the boxes with the lines that will be remove and a result of the explosion
       const linesToRemove = [
-        { box: topRightBoxNumber, lines: ["bottom", "left"] },
-        { box: topLeftBoxNumber, lines: ["bottom", "right"] },
-        { box: bottomRightBoxNumber, lines: ["top", "left"] },
-        { box: bottomLeftBoxNumber, lines: ["top", "right"] },
-        { box: topBox, lines: ["right", "bottom", "left"] },
-        { box: rightBox, lines: ["top", "bottom", "left"] },
-        { box: bottomBox, lines: ["top", "right", "left"] },
-        { box: leftBox, lines: ["top", "right", "bottom"] },
-        { box: `box${boxNumber}`, lines: ["top", "right", "bottom", "left"] }
-      ];
+        { box: `box${topRightBoxNumber}`, lines: ["bottom", "left"] },
+        { box: `box${topLeftBoxNumber}`, lines: ["bottom", "right"] },
+        { box: `box${bottomRightBoxNumber}`, lines: ["top", "left"] },
+        { box: `box${bottomLeftBoxNumber}`, lines: ["top", "right"] },
+        { box: `box${topBox}`, lines: ["right", "bottom", "left"] },
+        { box: `box${rightBox}`, lines: ["top", "bottom", "left"] },
+        { box: `box${bottomBox}`, lines: ["top", "right", "left"] },
+        { box: `box${leftBox}`, lines: ["top", "right", "bottom"] },
+        { box: `box${thisBox}`, lines: ["top", "right", "bottom", "left"] }
+      ].filter(data => data.box !== "boxnull");
+
       return {
         linesToRemove,
         boxesToExplode
@@ -59,39 +49,18 @@ const animalExplosions = {
         explodes animal column
         ex:    0 1 2 3 4 5
       */
-      const boxNumber = parseInt(box.replace("box", ""));
-      let boxesToExplode = [`box${boxNumber}`];
-      let keepGoingLeft = true;
-      let keepGoingRight = true;
-      let boxToCheck = boxNumber;
-      while(keepGoingLeft){
-        const leftBox = boxInfo.getLeftBox(boxToCheck);
-        if(leftBox){
-          boxesToExplode.push(leftBox)
-          boxToCheck = parseInt(leftBox.replace("box", ""));
-        } else {
-          keepGoingLeft = false;
-        }
-      }
-      while(keepGoingRight){
-        const rightBox = boxInfo.getRightBox(boxToCheck);
-        if(rightBox){
-          boxesToExplode.push(rightBox)
-          boxToCheck = parseInt(rightBox.replace("box", ""));
-        } else {
-          keepGoingRight = false;
-        }
-      }
-      const temp = boxesToExplode.filter(data => data);
-      const temp2 = task.removedDoublesFromArray(temp);
-      boxesToExplode = temp2;
+
+      const temp = cheetahSquares[box].filter(data => data !== null);
+      const boxesToExplode = temp.map(data => `box${data}`);
+
       const linesToRemove = [];
       boxesToExplode.forEach(data => {
         linesToRemove.push({
           box: data,
           lines: ["right", "left"]
         })
-      })
+      });
+
       return {
         linesToRemove,
         boxesToExplode
@@ -109,39 +78,18 @@ const animalExplosions = {
               24
               30
       */
-      const boxNumber = parseInt(box.replace("box", ""));
-      let boxesToExplode = [`box${boxNumber}`];
-      let keepGoingUp = true;
-      let keepGoingDown = true;
-      let boxToCheck = boxNumber;
-      while(keepGoingUp){
-        const topBox = boxInfo.getTopBox(boxToCheck);
-        if(topBox){
-          boxesToExplode.push(topBox)
-          boxToCheck = parseInt(topBox.replace("box", ""));
-        } else {
-          keepGoingUp = false;
-        }
-      }
-      while(keepGoingDown){
-        const bottomBox = boxInfo.getBottomBox(boxToCheck);
-        if(bottomBox){
-          boxesToExplode.push(bottomBox)
-          boxToCheck = parseInt(bottomBox.replace("box", ""));
-        } else {
-          keepGoingDown = false;
-        }
-      }
-      const temp = boxesToExplode.filter(data => data);
-      const temp2 = task.removedDoublesFromArray(temp);
-      boxesToExplode = temp2;
+
+      const temp = pantherSquares[box].filter(data => data !== null);
+      const boxesToExplode = temp.map(data => `box${data}`);
+
       const linesToRemove = [];
       boxesToExplode.forEach(data => {
         linesToRemove.push({
           box: data,
           lines: ["top", "bottom"]
         })
-      })
+      });
+
       return {
         linesToRemove,
         boxesToExplode

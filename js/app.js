95e9ab4672071208e1fdc064d736b36c9b93689c
Isaiah Harrison <isaiahharrison@Isaiahs-MacBook-Pro-2.local>
2019-04-14 14:11:49 -0400

    reset settings

--- js/app.js
+++ js/app.js
@@ -71,7 +71,7 @@ let currentPage = "homePage";
 let chanceToGiveAWayPoint;
 
 
-let reset_settings = false;
+let reset_settings = true;
 //determines if we are on the game board
 let on_game_board = false;
 
